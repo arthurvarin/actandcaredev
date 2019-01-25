32352e2d46c6caad9672b2147ca6c395f764113a
cvoisin <cvoisin@outlook.fr>
2019-01-25 12:18:00 +0100

    new admin

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -18,30 +18,30 @@ export default class Admin extends Component {
     ref.on('value', snap => {
       //https://css-tricks.com/intro-firebase-react/
       let newAccounts = [];
-      let newKeys=[]
+      let newKeys = []
       snap.forEach(
         child => {
           newAccounts.push(child.val())
           newKeys.push(child.key)
         })
-        this.setState({
-          //listKeys: this.state.listKeys.concat(child.key),
-          //listAccounts: this.state.listAccounts.concat(child.val()),
-          listAccounts:newAccounts,
-          listKeys:newKeys,
-          loading: false
-        })
+      this.setState({
+        //listKeys: this.state.listKeys.concat(child.key),
+        //listAccounts: this.state.listAccounts.concat(child.val()),
+        listAccounts: newAccounts,
+        listKeys: newKeys,
+        loading: false
+      })
     })
   }
 
-  validateAccount(uid,index, e) {
+  validateAccount(uid, index, e) {
     e.preventDefault();
     firebase.database().ref('users/' + uid).update(
       {
         statut: "valide"
       })
   }
-  rendreAdmin(uid,index, e) {
+  rendreAdmin(uid, index, e) {
     e.preventDefault();
     firebase.database().ref('users/' + uid).update(
       {
@@ -55,26 +55,43 @@ export default class Admin extends Component {
     else if (this.state.listAccounts !== "") return this.state.listAccounts.map((account, index) => {
       i++;
       return (
-        <div id={index}>
-          <tr>
-            {/* <th scope="col">UID: {this.state.listKeys[i]}</th> */}
-            <th scope="col">Email: {account.email}</th>
-            <th scope="col">Nom: {account.name}</th>
-            <th scope="col">RPPS: {account.RPPS}</th>
-            <th scope="col">Date de naissance: {account.bdate}</th>
-            <th scope="col">Rue: {account.rue}</th>
-            <th scope="col">Specialité: {account.specialite}</th>
-            <th scope="col">Téléphone: {account.tel}</th>
-            <th scope="col">Région: {account.region}</th>
-            <th scope="col">Ville: {account.ville}</th>
-            <th scope="col" className="alert alert-danger">Statut: {account.statut}</th>
-          </tr>
-          <button onClick={this.validateAccount.bind(this, this.state.listKeys[i],index)}>Valider </button>
-          <button onClick={this.rendreAdmin.bind(this, this.state.listKeys[i],index)}>Rendre admin</button>
-          <br></br>
-          <br></br>
-          <br></br>
-        </div>
+        // <div id={index}>
+        //   <tr>
+        //     {/* <th scope="col">UID: {this.state.listKeys[i]}</th> */}
+        //     <th scope="col">Email: {account.email}</th>
+        //     <th scope="col">Nom: {account.name}</th>
+        //     <th scope="col">RPPS: {account.RPPS}</th>
+        //     <th scope="col">Date de naissance: {account.bdate}</th>
+        //     <th scope="col">Rue: {account.rue}</th>
+        //     <th scope="col">Specialité: {account.specialite}</th>
+        //     <th scope="col">Téléphone: {account.tel}</th>
+        //     <th scope="col">Région: {account.region}</th>
+        //     <th scope="col">Ville: {account.ville}</th>
+        //     <th scope="col" className="alert alert-danger">Statut: {account.statut}</th>
+        //   </tr>
+        //   <button onClick={this.validateAccount.bind(this, this.state.listKeys[i], index)}>Valider </button>
+        //   <button onClick={this.rendreAdmin.bind(this, this.state.listKeys[i], index)}>Rendre admin</button>
+        //   <br></br>
+        //   <br></br>
+        //   <br></br>
+        // </div>
+
+        <tr id={index}>
+          {/* <th scope="col">UID: {this.state.listKeys[i]}</th> */}
+          <th>{account.email}</th>
+          <th>{account.name}</th>
+          <th>{account.RPPS}</th>
+          <th>{account.bdate}</th>
+          <th>{account.rue}</th>
+          <th>{account.specialite}</th>
+          <th>{account.tel}</th>
+          <th>{account.region}</th>
+          <th>{account.ville}</th>
+          <th>{account.statut}</th>
+          <th><button onClick={this.validateAccount.bind(this, this.state.listKeys[i], index)}>Valider </button>
+          <button onClick={this.rendreAdmin.bind(this, this.state.listKeys[i], index)}>Rendre admin</button></th>
+        </tr>
+
       )
 
     })
@@ -97,7 +114,23 @@ export default class Admin extends Component {
               <br></br>
               <br></br>
               <div>
-                {this.display()}
+                <table class="table table-striped">
+                  <tr>
+                    {/* <th scope="col">UID: {this.state.listKeys[i]}</th> */}
+                    <th scope="col">Email</th>
+                    <th scope="col">Nom</th>
+                    <th scope="col">RPPS</th>
+                    <th scope="col">Date de naissance</th>
+                    <th scope="col">Rue</th>
+                    <th scope="col">Specialité</th>
+                    <th scope="col">Téléphone</th>
+                    <th scope="col">Région</th>
+                    <th scope="col">Ville</th>
+                    <th scope="col">Statut</th>
+                    <th scope="col">Modifier</th>
+                  </tr>
+                  {this.display()}
+                </table>
               </div>
               <br></br>
             </div>
--- src/Components/RouterUNUSED.js
+++ src/Components/RouterUNUSED.js
@@ -1,59 +1,19 @@
-import React, {Component} from 'react';
-import { Route} from 'react-router-dom'
+import React from 'react';
+import { Route } from 'react-router-dom'
 import AjoutMissions from './AjoutMissions/AjoutMissions.js'
 import RechercheMissions from './RechercheMissions/RechercheMissions.js'
-import Signin from './Signin/Signin.js'
-import Signup from './Signup/Signup.js'
-import Test from './Test/Test.js'
 import MissionPage from './MissionPage/MissionPage.js'
-import UserCreated from './UserCreated/UserCreated.js';
-import Logout from './Logout/Logout.js'
-import PrivateRoute from './PrivateRoute.js'
-import * as firebase from 'firebase'
+import PageAccueil from './Accueil/PageAccueil.js'
 
-
-export default class Router extends Component {
-  state = { loading: true, authenticated: false, user: null };
-
-  componentDidMount() {
-
-    firebase.auth().onAuthStateChanged( (user)=> {
-      if (user) {
-        this.setState({
-          authenticated: true,
-          currentUser: user,
-        });
-       /*  return firebase.database().ref('/users/' + user.uid).once('value').then(function (snapshot) {
-          this.setState({
-            authenticated: true,
-            currentUser: user,
-            loading: false
-          });
-        }); */
-       
-      } else {
-        this.setState({
-          authenticated: false,
-          currentUser: null,
-        });
-      }
-    });
-  }
-
+export default class Router extends React.Component {
   render() {
-    const { authenticated } = this.state;
     return (
-      <div>
-        <PrivateRoute path="/" component={RechercheMissions} authenticated={authenticated}/ >
-        <PrivateRoute path="/ajoutmissions" component={AjoutMissions} authenticated={authenticated}/>
-        <PrivateRoute path="/recherchemissions" component={RechercheMissions} authenticated={authenticated} />
-        <Route path="/login" component={Signin} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/test" component={Test} />
-        <Route path="/usercreated" component={UserCreated} />
-        <Route path="/missionpage/:nomission" component={MissionPage} />
-        <Route path="/Logout" component={Logout} />
-      </div>
+        <div>
+          <Route path="/ajoutmissions" component={AjoutMissions}/>
+          <Route path="/recherchemissions" component={RechercheMissions}/>
+          <Route path="/missionpage/:nomission" component={MissionPage}/>
+          <Route path="/pageaccueil" component={PageAccueil}/>
+        </div>
     );
   }
 }
