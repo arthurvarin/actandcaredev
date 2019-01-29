a30af2b474c9490982f54198327332006349c641
DESKTOP-1HFC1SR\Vithiya <tvithiya@hotmail.fr>
2019-01-29 12:17:28 +0100

    Page recherche mission coté agence fini
    sans modal ODM/DEVIS multiples

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,34 +3,35 @@ import { NavLink } from 'react-router-dom'
 import './Navbar.css'
 import logo from '../../Icons/logo.png';
 
+
 export default class Navbar extends React.Component {
   render() {
     return (
-      <nav id="navigation">
-        <div class="container-navbar">
-          <div class="li-logo">
-            <NavLink  activeClassName="nav-link active" className="nav-link" to="/"><img src={logo} id="logoaccueil" alt="logo"/></NavLink>
+        <nav id="navigation">
+          <div class="container-navbar">
+            <div class="li-logo">
+              <NavLink  activeClassName="nav-link active" className="nav-link" to="/"><img src={logo} id="logoaccueil" alt="logo"/></NavLink>
+            </div>
+            <div class="li-option">
+              <NavLink activeClassName="nav-link active option" className="nav-link" to="/ajoutmissions">Ajouter des missions<span class="sr-only">(current)</span></NavLink>
+            </div>
+            <div class="li-option">
+              <NavLink activeClassName = "nav-link active option" className="nav-link"  to="/recherchemissions">Rechercher des missions</NavLink>
+            </div>
+            <div class="li-option">
+              <NavLink activeClassName = "nav-link active option" className="nav-link"  to="/inscriptions">Admin</NavLink>
+            </div>
+            <div class="li-option">
+              <NavLink activeClassName = "nav-link active option" className="nav-link"  to="/rm_medecin">Mode médecin</NavLink>
+            </div>
+            <div class="li-option-right">
+              <NavLink activeClassName = "nav-link active option" className="nav-link"  to="/logout">Déconnexion</NavLink>
+            </div>
+            <div class="li-option-right">
+              <NavLink activeClassName = "nav-link active option" className="nav-link"  to="/accoun">Mon compte</NavLink>
+            </div>
           </div>
-          <div class="li-option">
-            <NavLink activeClassName="nav-link active option" className="nav-link" to="/ajoutmissions">Ajouter des missions<span class="sr-only">(current)</span></NavLink>
-          </div>
-          <div class="li-option">
-            <NavLink activeClassName = "nav-link active option" className="nav-link"  to="/recherchemissions">Rechercher des missions</NavLink>
-          </div>
-          <div class="li-option">
-            <NavLink activeClassName = "nav-link active option" className="nav-link"  to="/inscriptions">Admin</NavLink>
-          </div>
-          <div class="li-option">
-            <NavLink activeClassName = "nav-link active option" className="nav-link"  to="/rm_medecin">Mode médecin</NavLink>
-          </div>
-          <div class="li-option-right">
-            <NavLink activeClassName = "nav-link active option" className="nav-link"  to="/logout">Déconnexion</NavLink>
-          </div>
-          <div class="li-option-right">
-            <NavLink activeClassName = "nav-link active option" className="nav-link"  to="/accoun">Mon compte</NavLink>
-          </div>
-        </div>
-      </nav>
+        </nav>
     );
   }
 }
