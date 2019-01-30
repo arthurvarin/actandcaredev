5585f624c849aa666c20500085ac596cf47dc13a
cvoisin <cvoisin@outlook.fr>
2019-01-30 15:23:46 +0100

    Modal détails coté médecin

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -12,9 +12,9 @@ var uiConfig = {
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+    //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    //firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
   ],
   credentialHelper: firebaseui.auth.CredentialHelper.NONE,
 
