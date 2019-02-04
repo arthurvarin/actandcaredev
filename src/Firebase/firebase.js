d45a84592de85854180e16d4edb5e2be987ef478
cvoisin <cvoisin@outlook.fr>
2019-02-04 12:04:51 +0100

    firebase rm

--- src/Firebase/firebase.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-    apiKey: "AIzaSyDlkUxdHRjfUNCNWX81I79YqSiQMpNs4s0",
-    authDomain: "actandcare-5cf3c.firebaseapp.com",
-    databaseURL: "https://actandcare-5cf3c.firebaseio.com",
-    projectId: "actandcare-5cf3c",
-    storageBucket: "actandcare-5cf3c.appspot.com",
-    messagingSenderId: "827295470651"
-};
